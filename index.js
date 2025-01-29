29c92150a3af3c08264d66c73247ae30d8860d87
aman9893 <aman9893jain@gmail.com>
2025-01-29 20:27:23 +0530

    add

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var router = express.Router();
 
 const cors = require('cors');
 var fs = require('fs');
@@ -13,7 +12,16 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 var server = require('http').createServer(app);
+var io = require('socket.io')(server, {
+  cors: {
+    origin: "http://localhost:4200",
+    origin: 'https://ondoorbackend.vercel.app/',
+    methods: ["GET", "POST"]
+  }
+})
 var serverPort = 3001;
+
+var user_socket_connect_list = [];
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -56,18 +64,52 @@ app.use(function(req, res, next) {
 
 app.get('/test', (req,res) => res.send('Hello World'))
 
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
-  });
+// import express inside dynamic added.
+fs.readdirSync('./controllers').forEach((file) => {
+  if (file.substr(-3) == ".js") {
+    route = require('./controllers/' + file);
+    route.controller(app, io, user_socket_connect_list);
+  }
+})
 
-  router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
-  });
-  
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
 module.exports = app;
 
 server.listen(serverPort);
 
 console.log("Server Start : " + serverPort );
 
+Array.prototype.swap = (x, y) => {
+  var b = this[x];
+  this[x] = this[y];
+  this[y] = b;
+  return this;
+}
 
+Array.prototype.insert = (index, item) => {
+  this.splice(index, 0, item);
+}
+
+Array.prototype.replace_null = (replace = '""') => {
+  return JSON.parse(JSON.stringify(this).replace(/null/g, replace));
+}
+
+String.prototype.replaceAll = (search, replacement) => {
+  var target = this;
+  return target.replace(new RegExp(search, 'g'), replacement);
+}
