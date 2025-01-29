3570af5d18cd13bbb046ecda1a051696f2ae447a
aman9893 <aman9893jain@gmail.com>
2025-01-29 20:37:14 +0530

    add

--- index.js
+++ index.js
@@ -46,21 +46,6 @@ app.set('views', path.join(__dirname,'views'));
 app.set('view engine','html');
 
 
-app.use((req, res, next) => {
-  res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
-  next();
-});
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-  app.use(cors());
-  app.all('*', function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-   next();
-});
 
 app.get('/test', (req,res) => res.send('Hello World'))
 
@@ -88,7 +73,6 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
 
 server.listen(serverPort);
 
@@ -113,3 +97,4 @@ String.prototype.replaceAll = (search, replacement) => {
   var target = this;
   return target.replace(new RegExp(search, 'g'), replacement);
 }
+module.exports = app;
